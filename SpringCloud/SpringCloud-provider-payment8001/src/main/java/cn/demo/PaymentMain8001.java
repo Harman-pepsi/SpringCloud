a1cbb77ccc8874435bc7282c8fdf94540617cf13
Harman <853696509@qq.com>
2020-07-20 17:23:22 +0800

    配置Eureka集群

--- SpringCloud/SpringCloud-provider-payment8001/src/main/java/cn/demo/PaymentMain8001.java
+++ SpringCloud/SpringCloud-provider-payment8001/src/main/java/cn/demo/PaymentMain8001.java
@@ -2,6 +2,7 @@ package cn.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Classname PaymentMain8001
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Since 1.0
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
