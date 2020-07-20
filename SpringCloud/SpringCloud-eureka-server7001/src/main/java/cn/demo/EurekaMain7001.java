fe47aeb452f546e4a7bd0f0cdd2116a8ae1c0444
Harman <853696509@qq.com>
2020-07-20 16:13:25 +0800

    开启Eureka服务

---
+++ SpringCloud/SpringCloud-eureka-server7001/src/main/java/cn/demo/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package cn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Classname EurekaMain7001
+ * @Description TODO
+ * @Date 2020/7/20 15:51
+ * @Created by XJC·AW
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
