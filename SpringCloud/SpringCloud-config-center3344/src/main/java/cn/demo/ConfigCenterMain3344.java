36d0c3bd07dc79197bb410f4321bff3dc4ad23e1
Harman <853696509@qq.com>
2020-07-25 01:14:12 +0800

    SpringCloud Config服务中心

---
+++ SpringCloud/SpringCloud-config-center3344/src/main/java/cn/demo/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package cn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Classname ConfigCenterMain3344
+ * @Description TODO
+ * @Date 2020/7/25 1:04
+ * @Created by XJC·AW
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
